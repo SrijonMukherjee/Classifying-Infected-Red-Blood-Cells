20c5482c6886d06d04c41832b6197fce275ce5c8
SrijonMukherjee <38012172+SrijonMukherjee@users.noreply.github.com>
2021-07-17 04:36:25 -0400

    Add files via upload

---
+++ predict_srijon.py
@@ -0,0 +1,20 @@
+import numpy as np
+from keras.models import load_model
+import cv2
+import os
+
+def predict(list_path):
+    # input will be a list of paths containing images to be classified
+    # %% --------------------------------------------- Data Prep -------------------------------------------------------
+    x=[]
+    RESIZE_TO=50
+    for path in list_path:
+        x.append(cv2.resize(cv2.imread(path), (RESIZE_TO, RESIZE_TO)))
+    x=np.array(x)
+    x = x.reshape(len(x), -1)
+    x = x / 255
+    # %% --------------------------------------------- Predict ---------------------------------------------------------
+    model = load_model('mlp_srijon.hdf5')
+    y_pred = np.argmax(model.predict(x), axis=1)
+    return y_pred, model
+
